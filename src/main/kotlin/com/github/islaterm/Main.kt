3c79d1e5a3085a001531e69022fcd3688d231d07
Ignacio Slater Muñoz <ignacio.slater@ug.uchile.cl>
2019-11-12 16:27:49 -0300

    Updated links

--- src/main/kotlin/com/github/islaterm/Main.kt
+++ src/main/kotlin/com/github/islaterm/Main.kt
@@ -2,14 +2,17 @@ package com.github.islaterm
 
 import me.ivmg.telegram.bot
 import me.ivmg.telegram.dispatch
+import me.ivmg.telegram.dispatcher.channel
 import me.ivmg.telegram.dispatcher.message
-import me.ivmg.telegram.dispatcher.text
+import me.ivmg.telegram.entities.Update
 import me.ivmg.telegram.extensions.filters.Filter
+import okhttp3.logging.HttpLoggingInterceptor
 import org.yaml.snakeyaml.Yaml
 import java.io.BufferedReader
 import java.io.File
 import java.io.InputStreamReader
 import java.util.regex.Pattern
+import kotlin.concurrent.timer
 
 val urlPattern: Pattern = Pattern.compile(
     "(?:^|[\\W])((ht|f)tp(s?)://|www\\.)(([\\w\\-]+\\.)+?([\\w\\-.~]+/?)*[\\p{Alnum}.,%_=?&#\\-+()\\[\\]*$~@!:/{};']*)",
@@ -17,43 +20,59 @@ val urlPattern: Pattern = Pattern.compile(
 )
 
 val runtime: Runtime = Runtime.getRuntime()
+val handler = UpdateHandler()
+val urls = mutableSetOf<String>()
+var lastUpdate: Long = System.currentTimeMillis()
+val linksFile = File("links.txt")
 
 fun main() {
-    var lastUpdate = System.currentTimeMillis()
-    val linksFile = File("links.txt")
     if (!linksFile.exists()) {
         linksFile.createNewFile()
     }
-    val urls = File("links.txt").readLines().toMutableSet()
     val key = Yaml().load<Map<String, String>>(File("src/main/resources/token.yaml").readText())
     val bot = bot {
+        logLevel = HttpLoggingInterceptor.Level.NONE
         token = key["token"] ?: error("")
         dispatch {
             message(Filter.All) { _, update ->
-                val text = update.message?.text ?: update.message?.caption ?: ""
-                val matcher = urlPattern.matcher(text)
-                while (matcher.find()) {
-                    val matchStart = matcher.start(1)
-                    val matchEnd = matcher.end()
-                    val link = text.substring(matchStart until matchEnd)
-                    urls.add(link)
-                }
-                val currentTime = System.currentTimeMillis()
-                if (currentTime != lastUpdate && (currentTime.rem(10000)) == 0L) {
-                    lastUpdate = currentTime
-                    linksFile.writeText(urls.joinToString(System.lineSeparator()))
-                    var process = runtime.exec("git.exe status")
-                    BufferedReader(InputStreamReader(process.inputStream)).lines().forEach { println(it) }
-                    process = runtime.exec("git.exe commit -a -m \"Updated links\" ")
-                    BufferedReader(InputStreamReader(process.inputStream)).lines().forEach { println(it) }
-                    process = runtime.exec("git.exe pull origin master")
-                    BufferedReader(InputStreamReader(process.inputStream)).lines().forEach { println(it) }
-                    process = runtime.exec("git.exe push origin master")
-                    BufferedReader(InputStreamReader(process.inputStream)).lines().forEach { println(it) }
-                }
+                handler.parseUpdate(update)
+            }
+            channel { _, update ->
+                handler.parseUpdate(update)
             }
         }
     }
+    timer("Git sync", period = 300_000L, )
     bot.startPolling()
+}
+
+class UpdateHandler {
+    fun parseUpdate(update: Update) {
+        synchronized(this) {
+            val text = update.channelPost?.text ?: update.channelPost?.caption ?: ""
+            val matcher = urlPattern.matcher(text)
+            while (matcher.find()) {
+                val matchStart = matcher.start(1)
+                val matchEnd = matcher.end()
+                val link = text.substring(matchStart until matchEnd)
+                urls.add(link)
+                println("Added $link")
+            }
+            val currentTime = System.currentTimeMillis()
+            if (currentTime > lastUpdate + 300_000) {
+                lastUpdate = currentTime
+                linksFile.writeText(urls.joinToString(System.lineSeparator()))
+                var process = runtime.exec("git.exe status")
+                BufferedReader(InputStreamReader(process.inputStream)).lines().forEach { println(it) }
+                process = runtime.exec("git.exe commit -a -m \"Updated links\" ")
+                BufferedReader(InputStreamReader(process.inputStream)).lines().forEach { println(it) }
+                process = runtime.exec("git.exe pull origin master")
+                BufferedReader(InputStreamReader(process.inputStream)).lines().forEach { println(it) }
+                process = runtime.exec("git.exe push origin master")
+                BufferedReader(InputStreamReader(process.inputStream)).lines().forEach { println(it) }
+                urls.clear()
+            }
+        }
+    }
 }
 
