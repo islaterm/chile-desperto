b9993ad61f3a84e73ccf5cf73045dfcee4ef0385
Ignacio Slater Muñoz <ignacio.slater@ug.uchile.cl>
2019-11-12 17:01:16 -0300

    Updated links

--- src/main/kotlin/com/github/islaterm/Main.kt
+++ src/main/kotlin/com/github/islaterm/Main.kt
@@ -13,6 +13,8 @@ import java.io.File
 import java.io.InputStreamReader
 import java.util.regex.Pattern
 import kotlin.concurrent.timer
+import java.util.TimerTask
+
 
 val urlPattern: Pattern = Pattern.compile(
     "(?:^|[\\W])((ht|f)tp(s?)://|www\\.)(([\\w\\-]+\\.)+?([\\w\\-.~]+/?)*[\\p{Alnum}.,%_=?&#\\-+()\\[\\]*$~@!:/{};']*)",
@@ -22,7 +24,6 @@ val urlPattern: Pattern = Pattern.compile(
 val runtime: Runtime = Runtime.getRuntime()
 val handler = UpdateHandler()
 val urls = mutableSetOf<String>()
-var lastUpdate: Long = System.currentTimeMillis()
 val linksFile = File("links.txt")
 
 fun main() {
@@ -35,21 +36,30 @@ fun main() {
         token = key["token"] ?: error("")
         dispatch {
             message(Filter.All) { _, update ->
-                handler.parseUpdate(update)
+                val text = update.message?.text ?: update.message?.caption ?: ""
+                handler.parseUpdate(text)
             }
             channel { _, update ->
-                handler.parseUpdate(update)
+                val text = update.channelPost?.text ?: update.channelPost?.caption ?: ""
+                handler.parseUpdate(text)
             }
         }
     }
-    timer("Git sync", period = 300_000L, )
+    timer("Git sync", period = 10L, action = object : TimerTask(), (TimerTask) -> Unit {
+        override fun run() {
+            handler.sync()
+        }
+
+        override fun invoke(p1: TimerTask) {
+            handler.sync()
+        }
+    })
     bot.startPolling()
 }
 
 class UpdateHandler {
-    fun parseUpdate(update: Update) {
+    fun parseUpdate(text: String) {
         synchronized(this) {
-            val text = update.channelPost?.text ?: update.channelPost?.caption ?: ""
             val matcher = urlPattern.matcher(text)
             while (matcher.find()) {
                 val matchStart = matcher.start(1)
@@ -58,20 +68,21 @@ class UpdateHandler {
                 urls.add(link)
                 println("Added $link")
             }
-            val currentTime = System.currentTimeMillis()
-            if (currentTime > lastUpdate + 300_000) {
-                lastUpdate = currentTime
-                linksFile.writeText(urls.joinToString(System.lineSeparator()))
-                var process = runtime.exec("git.exe status")
-                BufferedReader(InputStreamReader(process.inputStream)).lines().forEach { println(it) }
-                process = runtime.exec("git.exe commit -a -m \"Updated links\" ")
-                BufferedReader(InputStreamReader(process.inputStream)).lines().forEach { println(it) }
-                process = runtime.exec("git.exe pull origin master")
-                BufferedReader(InputStreamReader(process.inputStream)).lines().forEach { println(it) }
-                process = runtime.exec("git.exe push origin master")
-                BufferedReader(InputStreamReader(process.inputStream)).lines().forEach { println(it) }
-                urls.clear()
-            }
+        }
+    }
+
+    fun sync() {
+        synchronized(this) {
+            linksFile.appendText(urls.joinToString(System.lineSeparator()))
+            var process = runtime.exec("git.exe status")
+            BufferedReader(InputStreamReader(process.inputStream)).lines().forEach { println(it) }
+            process = runtime.exec("git.exe commit -a -m \"Updated links\" ")
+            BufferedReader(InputStreamReader(process.inputStream)).lines().forEach { println(it) }
+            process = runtime.exec("git.exe pull origin master")
+            BufferedReader(InputStreamReader(process.inputStream)).lines().forEach { println(it) }
+            process = runtime.exec("git.exe push origin master")
+            BufferedReader(InputStreamReader(process.inputStream)).lines().forEach { println(it) }
+            urls.clear()
         }
     }
 }
